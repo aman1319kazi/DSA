061c43e9eb53a3829461e7121f79bf04f03b3deb
aman1319kazi <amankazi1319@gmail.com>
2021-06-13 21:35:36 +0530

    Added insert_at_beginning and insert_at_end function in linkedlist

--- DataStructures/LinkedList/ll.py
+++ DataStructures/LinkedList/ll.py
@@ -11,3 +11,15 @@ class linkedlist:
 	def insert_at_beginning(self, data):
 		self.head = node(data, self.head)
 
+	#add node at last location
+	def insert_at_end(self, data):
+		if not self.head:
+			self.insert_at_beginning(data)
+			return
+
+		temp = self.head
+		while temp.next:
+			temp = temp.next
+
+		temp.next = node(data)
+
