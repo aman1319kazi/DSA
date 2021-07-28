807f51d371c58f6500a221eca57057e894692c63
aman1319kazi <amankazi1319@gmail.com>
2021-07-28 23:39:48 +0530

    Changed linkedlist code

--- DataStructures/LinkedList/ll.py
+++ DataStructures/LinkedList/ll.py
@@ -60,15 +60,55 @@ class linkedlist:
 		return counter
 
 	#remove node at first location
+	def remove_first(self):
+		if self.head:
+			self.head = self.head.next
+		else:
+			print("no node in ll")
+			return
 
 	#remove node at last location
+	def remove_last(self):
+		if not self.head:
+			print("no node in ll")
+			return
+
+		elif self.get_length()>1:
+			temp = self.head
+			while temp.next.next:
+				temp = temp.next
+			temp.next = None
+			return
+
+		elif self.get_length()==1:
+			self.head = None
 
 	#remove node at any location
+	def remove_at_index(self, index):
+		if not self.head:
+			print("no node in ll to remove")
+			return
+
+		else:
+			if index == 0:
+				self.remove_first()
+				return
+
+			elif index>=1 and index<self.get_length():
+				temp = self.head
+				counter = 0
+				while temp.next.next: 
+					counter+=1
+					if index == counter:
+						break
+					temp = temp.next
+				temp.next = temp.next.next
+
 
 	#display linkedlist in order
 	def display(self):
 		if not self.head:
-			return "Empty linkedlist"
+			print("Empty linkedlist")
 
 		temp = self.head
 		while temp:
@@ -80,4 +120,7 @@ li.insert_at_beginning(3)
 li.insert_at_end(13)
 li.insert_at_end(30)
 li.insert_at_index(-2,32)
+li.display()
+li.remove_at_index(1)
+print()
 li.display()
