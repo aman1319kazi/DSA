78ba2d15eac180ad0353298a6e43ddf16b032983
aman1319kazi <amankazi1319@gmail.com>
2021-11-05 19:14:29 +0530

    Added complete implementation of binary search tree

--- DataStructures/Trees/Binary Search Tree/Binary.py
+++ DataStructures/Trees/Binary Search Tree/Binary.py
@@ -19,25 +19,91 @@ class BST:
 				self.right = BST(data)
 
 	def print_tree(self, li=[]):
-		li.append(self.data)
-
 		if self.left:
 			self.left.print_tree(li)
+		print(self.data)
 		if self.right:
 			self.right.print_tree(li)
 		return li
 
+	def search(self, val):
+		if val==self.data:
+			return True
 
-t = BST(17)
-# t.add_child(2)
-# t.add_child(3)
-# t.add_child(4)
-# t.add_child(5)
-t.add_child(4)
-t.add_child(1)
-t.add_child(20)
-t.add_child(9)
-t.add_child(23)
-t.add_child(18)
-t.add_child(34)
-print(t.print_tree())
+		if val<self.data:
+			if self.left:
+				return self.left.search(val)
+			else:
+				return False
+
+		if val>self.data:
+			if self.right:
+				return self.right.search(val)
+			else:
+				return False
+
+
+	def min(self):
+		minimum = self.data
+		while self.left:
+			# print(minimum)
+			minimum = self.left.data
+			self.left = self.left.left
+		return minimum
+
+	def minrecur(self):
+		if not self.left:
+			return self.data
+		return self.left.minrecur()
+
+	def max(self):
+		maximum = self.data
+		while self.right:
+			# print(maximum)
+			maximum = self.right.data
+			self.right = self.right.right
+		return maximum
+
+	def maxrecur(self):
+		if not self.right:
+			return self.data
+		return self.right.maxrecur
+
+	def delete(self, value):
+		if value<self.data:
+			if self.left:
+				self.left = self.left.delete(value)
+
+		elif value>self.data:
+			if self.right:
+				self.right = self.right.delete(value)
+
+		else:
+
+			if self.left is None and self.right is None:
+				return None
+
+			if self.left is None:
+				return self.right
+
+			if self.right is None:
+				return self.left
+
+			minval = self.right.minrecur()
+			self.data = minval
+			self.right = self.right.delete(minval)
+
+		return self
+
+
+def build(elements):
+	root = BST(elements[0])
+	for x in range(1, len(elements)):
+		root.add_child(elements[x])
+
+	return root
+
+tree = build([7, 8, 9, 10, 11, 12, 13])
+print(tree.print_tree())
+print(tree.min())
+print(tree.max())
