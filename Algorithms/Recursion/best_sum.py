84ce6e5e3fd27847ebb41d8519b6290932497cd3
aman1319kazi <amankazi1319@gmail.com>
2021-11-17 23:08:10 +0530

    Added memoization of best sum

--- Algorithms/Recursion/best_sum.py
+++ Algorithms/Recursion/best_sum.py
@@ -1,4 +1,6 @@
 def best_sum(target, arr, memo={}):
+    if target in memo:
+        return memo[target]
     if target < 0:
         return None
     if target == 0:
@@ -7,15 +9,16 @@ def best_sum(target, arr, memo={}):
     min_combination = None
     for x in arr:
         new_target = target - x
-        combination = best_sum(new_target, arr)
+        combination = best_sum(new_target, arr, memo)
         if combination is not None:
             temp = combination + [x]
             if min_combination is None or len(temp)<len(min_combination):
                 min_combination = temp
 
+    memo[target] = min_combination
     return min_combination
 print(best_sum(7, [2, 3, 6, 7], {}))
 print(best_sum(7, [5, 3, 4, 7], {}))
 print(best_sum(7, [2, 4], {}))
 print(best_sum(8, [2, 3, 5], {}))
-print(best_sum(300, [7, 14], {}))
+print(best_sum(100, [1, 2, 5, 25], {}))
