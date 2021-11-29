88935169404e125c69cb3f6b582b189563e6317b
aman1319kazi <amankazi1319@gmail.com>
2021-11-30 00:26:15 +0530

    Corrected binary search recursion solution

--- Algorithms/Binary Search/binary search/binary_search_recursion.py
+++ Algorithms/Binary Search/binary search/binary_search_recursion.py
@@ -1,11 +1,11 @@
 def search(arr, l, r, ele):
-    if l < r:
+    if l <= r:
         mid = (l + r) // 2
         if arr[mid] == ele:
             return mid
 
         elif ele < arr[mid]:
-            return search(arr, l, mid, ele)
+            return search(arr, l, mid - 1, ele)
 
         elif ele > arr[mid]:
             return search(arr, mid + 1, r, ele)
@@ -14,5 +14,5 @@ def search(arr, l, r, ele):
         return -1
 
 arr = [1, 2, 3, 4, 5, 6]
-ele = 5
-print(search(arr, 0, len(arr), ele))
+ele = 4
+print(search(arr, 0, len(arr) - 1, ele))
