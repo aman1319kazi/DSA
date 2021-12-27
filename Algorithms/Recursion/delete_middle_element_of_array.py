c87e67e5557be16548938c967b7f4428215beb92
aman1319kazi <amankazi1319@gmail.com>
2021-12-27 18:43:09 +0530

    Changed delete_middle_element_of_array recursion solution

--- Algorithms/Recursion/delete_middle_element_of_array.py
+++ Algorithms/Recursion/delete_middle_element_of_array.py
@@ -1,15 +1,16 @@
 def delete_middle(arr, middle):
-	if not arr:
-		return
+    if not arr:
+        return
 
-	if middle == 0:
-		arr.pop()
-		return 
-		
-	temp = arr.pop()
-	delete_middle(arr, middle-1)
-	arr.append(temp)
+    if middle == 0:
+        arr.pop()
+        return
 
-arr = [6, 5, 4, 3, 2, 1]
-delete_middle(arr, len(arr)//2)
+    temp = arr.pop()
+    delete_middle(arr, middle-1)
+    arr.append(temp)
+
+arr = []
+
+delete_middle(arr, len(arr) // 2)
 print(arr)
