cce3f1ad7e65b9aa181fab057d79719d79f919d3
aman1319kazi <amankazi1319@gmail.com>
2022-05-09 21:43:10 +0530

    Added Fibonacci memoized solution

---
+++ Algorithms/Dynamic Programming/Fibonacci/Fibonacci_memoized.py
@@ -0,0 +1,17 @@
+def fibonacci(number, memo):
+	if number in memo:
+		return memo[number]
+
+	if number < 2:
+		memo[number] = number
+		return number
+
+	memo[number] =  fibonacci(number - 1, memo) + fibonacci(number - 2, memo)
+	return memo[number]
+
+print(fibonacci(5, {}))
+print(fibonacci(10, {}))
+print(fibonacci(15, {}))
+print(fibonacci(20, {}))
+print(fibonacci(30, {}))
+print(fibonacci(40, {}))
